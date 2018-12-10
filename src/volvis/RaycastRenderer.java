935f2242936330714d50fee68c1069d9e3741380
Daan <daan-boer@hotmail.com>
2018-12-10 10:32:02 +0100

    fronttoback test

--- src/volvis/RaycastRenderer.java
+++ src/volvis/RaycastRenderer.java
@@ -529,6 +529,120 @@ public class RaycastRenderer extends Renderer implements TFChangeListener {
         }
     }
 
+    void compositeFront(double[] viewMatrix, int imageScaling, int depthScaling) {
+        // clear image
+        for (int j = 0; j < image.getHeight(); j++) {
+            for (int i = 0; i < image.getWidth(); i++) {
+                image.setRGB(i, j, 0);
+            }
+        }
+
+        // vector uVec and vVec define a plane through the origin,
+        // perpendicular to the view vector viewVec
+        double[] viewVec = new double[3];
+        double[] uVec = new double[3];
+        double[] vVec = new double[3];
+        VectorMath.setVector(viewVec, viewMatrix[2], viewMatrix[6], viewMatrix[10]);
+        VectorMath.setVector(uVec, viewMatrix[0], viewMatrix[4], viewMatrix[8]);
+        VectorMath.setVector(vVec, viewMatrix[1], viewMatrix[5], viewMatrix[9]);
+
+        // image is square
+        int imageCenter = image.getWidth() / 2;
+
+        double[] pixelCoord = new double[3];
+        double[] volumeCenter = new double[3];
+        VectorMath.setVector(volumeCenter, (double)volume.getDimX() / 2., (double)volume.getDimY() / 2., (double)volume.getDimZ() / 2.);
+
+        // sample on a plane through the origin of the volume data
+        double max = volume.getMaximum();
+        TFColor voxelColor = new TFColor();
+
+        int viewDim = (int) Math.sqrt(Math.pow((double) volume.getDimZ(), 2.) +
+                Math.pow((double) volume.getDimX(), 2.) +
+                Math.pow((double) volume.getDimY(), 2.) );
+
+        TFColor current, composite;
+
+        for (int j = 0; j < image.getHeight(); j+=imageScaling) {
+            for (int i = 0; i < image.getWidth(); i+=imageScaling) {
+
+                composite = new TFColor(0., 0., 0., 1.);
+
+                for (int k = 0; k < viewDim; k += depthScaling) {
+
+                    pixelCoord[0] = uVec[0] * (i - imageCenter) + vVec[0] * (j - imageCenter)
+                            + viewVec[0] * (k - volumeCenter[0]) + volumeCenter[0];
+                    pixelCoord[1] = uVec[1] * (i - imageCenter) + vVec[1] * (j - imageCenter)
+                            + viewVec[1] * (k - volumeCenter[1]) + volumeCenter[1];
+                    pixelCoord[2] = uVec[2] * (i - imageCenter) + vVec[2] * (j - imageCenter)
+                            + viewVec[2] * (k - volumeCenter[2]) + volumeCenter[2];
+
+                    current = tFunc.getColor(getVoxelTriLinear(pixelCoord));
+
+                    composite.r = composite.r + composite.a * current.a * current.r;
+                    composite.g = composite.g + composite.a * current.a * current.g;
+                    composite.b = composite.b + composite.a * current.a * current.b;
+                    composite.a = (1. - current.a) * composite.a;
+
+                    /*
+                    composite.r = composite.r + (1 - composite.a) * current.r;
+                    composite.g = composite.g + (1 - composite.a) * current.g;
+                    composite.b = composite.b + (1 - composite.a) * current.b;
+                    composite.a = composite.a + (1 - composite.a) * current.a;
+                    */
+
+                    if (composite.a < 0.05) {
+                        break;
+                    }
+                }
+
+                voxelColor.r = composite.r;
+                voxelColor.g = composite.g;
+                voxelColor.b = composite.b;
+                voxelColor.a = composite.a;
+
+                // BufferedImage expects a pixel color packed as ARGB in an int
+                int c_alpha = voxelColor.a <= 1.0 ? (int) Math.floor(voxelColor.a * 255) : 255;
+                int c_red = voxelColor.r <= 1.0 ? (int) Math.floor(voxelColor.r * 255) : 255;
+                int c_green = voxelColor.g <= 1.0 ? (int) Math.floor(voxelColor.g * 255) : 255;
+                int c_blue = voxelColor.b <= 1.0 ? (int) Math.floor(voxelColor.b * 255) : 255;
+                int pixelColor = (c_alpha << 24) | (c_red << 16) | (c_green << 8) | c_blue;
+
+                if (imageScaling > 1) {
+                    int odd = imageScaling % 2;
+
+                    if (odd == 0) {
+                        int half = imageScaling/2;
+
+                        for (int i_k = -half + 1; i_k <= half; ++i_k) {
+                            for (int j_k = -half + 1; j_k <= half; ++j_k) {
+                                if ((i + i_k >= 0) && (i + i_k < image.getWidth()) &&
+                                        (j + j_k >= 0) && (j + j_k) < image.getHeight()) {
+
+                                    image.setRGB(i + i_k, j + j_k, pixelColor);
+                                }
+                            }
+                        }
+                    } else {
+                        int half = (imageScaling-1)/2;
+
+                        for (int i_k = -half; i_k <= half; ++i_k) {
+                            for (int j_k = -half; j_k <= half; ++j_k) {
+                                if ((i + i_k >= 0) && (i + i_k < image.getWidth()) &&
+                                        (j + j_k >= 0) && (j + j_k) < image.getHeight()) {
+
+                                    image.setRGB(i + i_k, j + j_k, pixelColor);
+                                }
+                            }
+                        }
+                    }
+                } else {
+                    image.setRGB(i, j, pixelColor);
+                }
+            }
+        }
+    }
+
     void transfer2D(double[] viewMatrix, int imageScaling, int depthScaling) {
         // clear image
         for (int j = 0; j < image.getHeight(); j++) {
@@ -946,7 +1060,8 @@ public class RaycastRenderer extends Renderer implements TFChangeListener {
         } else if (panel.mode == 1) {
             mip(viewMatrix, imageScaling, depthScaling);
         } else if (panel.mode == 2) {
-            composite(viewMatrix, imageScaling, depthScaling);
+            //composite(viewMatrix, imageScaling, depthScaling);
+            compositeFront(viewMatrix, imageScaling, depthScaling);
         } else if (panel.mode == 3) {
 
             if (panel.shade) {
